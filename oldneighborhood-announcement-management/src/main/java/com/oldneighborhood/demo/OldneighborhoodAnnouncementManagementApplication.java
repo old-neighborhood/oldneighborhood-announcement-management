6e6a7960eee87ce4bf219fff8d39b9a16df06868
RenBrian <Brian_Ren@outlook.com>
2018-03-21 17:32:09 +0800

    "push"

--- oldneighborhood-announcement-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodAnnouncementManagementApplication.java
+++ oldneighborhood-announcement-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodAnnouncementManagementApplication.java
@@ -3,6 +3,7 @@ package com.oldneighborhood.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OldneighborhoodAnnouncementManagementApplication {
 

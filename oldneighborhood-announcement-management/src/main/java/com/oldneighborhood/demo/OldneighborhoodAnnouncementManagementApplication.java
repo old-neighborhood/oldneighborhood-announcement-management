5d85687c2798cb4e8d1fe4bd5c47ce4874341672
RenBrian <Brian_Ren@outlook.com>
2018-04-11 12:28:01 +0800

    "add EUREKA"

--- oldneighborhood-announcement-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodAnnouncementManagementApplication.java
+++ oldneighborhood-announcement-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodAnnouncementManagementApplication.java
@@ -2,9 +2,15 @@ package com.oldneighborhood.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@ComponentScan("com.oldneighborhood.demo")
 public class OldneighborhoodAnnouncementManagementApplication {
 
 	public static void main(String[] args) {
